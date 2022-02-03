041bbfbae53fb12075ae17c417f12c4eef1fe18a
Robert Haase <haesleinhuepf@users.noreply.github.com>
2022-02-03 20:12:45 +0100

    filter out non-processing steps before saving

--- src/napari_workflows/_io_yaml_v1.py
+++ src/napari_workflows/_io_yaml_v1.py
@@ -1,7 +1,17 @@
+from ._workflow import Workflow
+import numpy as np
+
 def save_workflow(workflow, filename):
+    # Filter out workflow steps that do not represent a processing step
+    workflow_to_save = Workflow()
+    for key, value in workflow._tasks.items():
+        if not isinstance(value[0], np.ndarray):
+            workflow_to_save.set(key, value)
+    
+    # Save the remaining steps to disk
     from yaml import dump
     with open(filename, 'w') as stream:
-        dump(workflow,stream)
+        dump(workflow_to_save,stream)
 
 def load_workflow(filename):
     from yaml import unsafe_load
